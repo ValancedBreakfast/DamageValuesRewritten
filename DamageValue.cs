d14274174d9937d69feb68be880c9dede8d688d1
Nikki Valens <34880412+ValancedBreakfast@users.noreply.github.com>
2025-09-27 02:00:02 -0500

    TickValue and fury red

--- DamageValue.cs
+++ DamageValue.cs
@@ -22,8 +22,37 @@ namespace DamageValues
             _rb.velocity = Vector2.up * 2.5f;
             yield return new WaitUntil(() =>
             {
+                Color c = (NailSlash.Reflection.fury ? new Color(1, 0.7, 0.7, opacity) : new Color(1, 1, 1, opacity));
                 foreach (SpriteRenderer rend in _renderers)
-                    rend.color = new Color(1, 1, 1, opacity);
+                    rend.color = c;
+                opacity -= Time.deltaTime * fadeRate;
+                return opacity <= 0;
+            });
+            Destroy(gameObject);
+        }
+    }
+    
+    internal class TickValue : MonoBehaviour
+    {
+        private SpriteRenderer[] _renderers;
+        private Rigidbody2D _rb;
+        private void Awake()
+        {
+            _rb = gameObject.AddComponent<Rigidbody2D>();
+            _rb.isKinematic = true;
+            _renderers = GetComponentsInChildren<SpriteRenderer>();
+            StartCoroutine(FadeOut());
+        }
+
+        private IEnumerator FadeOut()
+        {
+            float opacity = 1.0f;
+            float fadeRate = 2;
+            _rb.velocity = Vector2.up * 2.5f;
+            yield return new WaitUntil(() =>
+            {
+                foreach (SpriteRenderer rend in _renderers)
+                    rend.color = new Color(1, 1, 0.3, opacity);
                 opacity -= Time.deltaTime * fadeRate;
                 return opacity <= 0;
             });
